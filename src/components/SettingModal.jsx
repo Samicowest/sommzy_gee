e94d2de667a3250627fdf07619744038888c4ae8
Samicowest <samicowest@gmail.com>
2024-11-06 12:02:34 +0100

    removing dependencies not needed

--- src/components/SettingModal.jsx
+++ src/components/SettingModal.jsx
@@ -1,23 +1,8 @@
 import React, { useState } from "react";
 import { FaCheck } from "react-icons/fa";
 
-import av1 from "../assets/avatar/av1.jpg";
-import av3 from "../assets/avatar/av3.jpg";
-import av5 from "../assets/avatar/av5.jpg";
-import av6 from "../assets/avatar/av6.jpg";
-import av7 from "../assets/avatar/av7.jpg";
-import av8 from "../assets/avatar/av8.jpg";
-
-const data = [
-  { image: av1, key: 1 },
-  { image: av3, key: 3 },
-  { image: av5, key: 5 },
-  { image: av6, key: 6 },
-  { image: av7, key: 7 },
-  { image: av8, key: 8 },
-];
-
 import { MdClose } from "react-icons/md";
+import { data } from "../data";
 
 function SettingModal({ setModalOpen }) {
   const [isActive, setIsActive] = useState(1);
