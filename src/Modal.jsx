fefd7fef6b3ff6277df32df707c5a8a3b368b522
Samicowest <samicowest@gmail.com>
2024-11-06 12:02:13 +0100

    removing dependencies not needed

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,14 +1,5 @@
 import React from "react";
-import av1 from "./assets/avatar/av1.jpg";
-import av2 from "./assets/avatar/av2.jpg";
-import av3 from "./assets/avatar/av3.jpg";
-import av4 from "./assets/avatar/av4.jpg";
-import av5 from "./assets/avatar/av5.jpg";
-import av6 from "./assets/avatar/av6.jpg";
-import av7 from "./assets/avatar/av7.jpg";
-import av8 from "./assets/avatar/av8.jpg";
 import ConnectModal from "./components/ConnectModal";
-import ManualModal from "./components/manualModal";
 import SettingModal from "./components/SettingModal";
 
 function Modal({ modal, setModalOpen }) {
